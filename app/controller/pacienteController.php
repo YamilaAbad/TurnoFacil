bb09b6d42ddb6735cb6aacc59ff08d43a7c8cdeb
M Victoria <mvittoria7@gmail.com>
2022-10-10 21:05:32 -0300

    TUR-4: Creación base para el filtro de la lista medicos

--- app/controller/pacienteController.php
+++ app/controller/pacienteController.php
@@ -1,5 +1,7 @@
 <?php
 
+    include_once 'app/models/pacienteModel.php';
+    include_once 'app/views/pacienteView.php';
 
 class PacienteController {
 
@@ -7,9 +9,20 @@ class PacienteController {
     private $view;
 
     function __construct() {
+        $this->view = new PacienteView();
+        $this->model = new PacienteModel();
     
- 
+
     }
+    /* "Como paciente quiero ver la lista de médicos para seleccionar
+    uno"
+    CRITERIO DE ACEPTACIÓN:
+        -Seleccionar un médico
+        -Permitir filtrar médicos por especialidad y obra social que trabaja
+        -Mostrar días y horarios que atiende un médico
+    */
+
+
 
 
 
--- app/views/pacienteViews.php
+++ app/views/pacienteViews.php
@@ -1,9 +1,10 @@
 <?php
 
+require_once('libs/smarty/libs/Smarty.class.php');
 
 class PacienteViews{
-
-   
+    
+    /*Obtener dias y horarios que atiende un médico */
 
 
 
