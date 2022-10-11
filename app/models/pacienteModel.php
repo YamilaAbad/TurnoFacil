0825fc66720599413d5c77d3c63b6a2e71879510
noelia2016 <noeliacarrizo22@gmail.com>
2022-10-11 17:23:43 -0300

    se efectua la conexion con el modelo paciente falta terminar de obtener los horarios de filtro

--- app/models/pacienteModel.php
+++ app/models/pacienteModel.php
@@ -1,13 +1,28 @@
 <?php
 
-
+include_once 'app/helpers/DB.helper.php';
 class PacienteModel {
 
- 
+    private $db;
+    private $dbHelper;
+
+    function __construct() {
+        
+        $this->dbHelper = new DBHelper();
+        // me conecto a la BD
+        $this->db = $this->dbHelper->connect();
+    }
+
     /**
      * Obtiene los dias de atencion con turnos disponibles para el rango elegido de un medico en particular
      */
-    function obtenerHorariosDeAtencion($randoElegidoD, $rangoElegidoH, $turno, $medico){
+    function obtenerHorariosDeAtencion($rangoElegidoD, $rangoElegidoH, $turno, $medico){
+
+        $query = $this->db->prepare("SELECT * FROM turno
+        WHERE fecha_disponible between ? and ? and turno = ? and turno_id_medico= ? ORDER BY  AS;");
+        $query->execute([$rangoElegidoD, $rangoElegidoH, $turno, $medico]);
+        $turnos = $query->fetchAll(PDO::FETCH_OBJ);
+        return $turnos;
 
     }
 
