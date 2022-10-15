81d89d35a33b64ed7bb35c9f69c8122730dad3ac
YamilaAbad <yamiabad@hotmail.com>
2022-10-15 17:54:56 -0300

    TUR-4 agregado de filtro con doble opción de busqueda (obra social y especialidad)

--- app/models/pacienteModel.php
+++ app/models/pacienteModel.php
@@ -39,12 +39,26 @@
                                         INNER JOIN medico m ON (u.usuario_id = m.medico_usuario_id)
                                         INNER JOIN rango_horario r ON (m.medico_id_rango_horario = r.rango_id)
                                         INNER JOIN medico_os med ON (u.usuario_id = med.mos_id_medico)
-                                        WHERE med.mos_id_obrasocial = ( SELECT o.os_id FROM obra_social o WHERE os_id = ?)');
+                                        WHERE med.mos_id_obrasocial = ( SELECT o.os_id FROM obra_social o WHERE o.os_id = ?)');
             $query->execute([$id]);
             $medico=$query->fetch(PDO :: FETCH_OBJ);
             return $medico;
     }
-}
 
+    /******************************OBTENEMOS DIAS, HORARIOS Y MEDICOS FILTADROS POR OBRA SOCIAL Y ESPECIALIDAD*********************/
+    //revisar pasajes de id - sujeto a mejoras -
+        function getAll($id1, $id2){
+            $query=$this->db->prepare('SELECT u.usuario_nombre, u.usuario_apellido, r.rango_horario_inicial, r.rango_horario_final, r.rango_dia
+                                        FROM usuario u 
+                                        INNER JOIN medico m ON (u.usuario_id = m.medico_usuario_id)
+                                        INNER JOIN rango_horario r ON (m.medico_id_rango_horario = r.rango_id)
+                                        INNER JOIN medico_os med ON (u.usuario_id = med.mos_id_medico)
+                                        WHERE med.mos_id_obrasocial = ( SELECT o.os_id FROM obra_social o WHERE o.os_id = ? AND m.medico_id_especialidad = ?)');
+            $query->execute([$id1,$id2]);
+            $medico=$query->fetch(PDO :: FETCH_OBJ);
+            return $medico;
+    
+        }
+    }
 ?>
 
